d851499b6086d8c9651ab501ec302f08e57d6714
Thallis Ferreira <thallis.ferreira2019@gmail.com>
2022-07-27 16:06:09 -0300

    crated Department entity class

---
+++ src/aplication/Program.java
@@ -0,0 +1,10 @@
+package aplication;
+
+import model.entities.Department;
+
+public class Program {
+	public static void main(String[] args) {
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+	}
+}
