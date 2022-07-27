fc4345d92c58af746976154271a49b80165d1560
Thallis Ferreira <thallis.ferreira2019@gmail.com>
2022-07-27 16:14:18 -0300

    Create Seller entity class

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,16 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		
+		System.out.println(seller);
+		
 	}
 }
