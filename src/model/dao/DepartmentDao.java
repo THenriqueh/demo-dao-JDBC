1ddcc12fe8b8153dfd8556e987f8113fb97edff7
Thallis Ferreira <thallis.ferreira2019@gmail.com>
2022-07-27 16:21:41 -0300

    Created intrfaces DpartmentDao and SellerDao

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,19 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+	
+	void insert(Department obj);
+	void update(Department obj);
+	void deletById (Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
+	
+	
+	
+	
+
+}
