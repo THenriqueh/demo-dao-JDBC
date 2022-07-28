570bbf5992b4f66d211062155aa21d64b657ba27
Thallis Ferreira <thallis.ferreira2019@gmail.com>
2022-07-28 09:12:37 -0300

    DepartmentDao implementation and test

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -8,7 +8,7 @@ public interface DepartmentDao {
 	
 	void insert(Department obj);
 	void update(Department obj);
-	void deletById (Integer id);
+	void deleteById (Integer id);
 	Department findById(Integer id);
 	List<Department> findAll();
 	
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -118,7 +118,7 @@ public class SellerDaoJDBC implements SellerDao {
 		ResultSet rs = null;
 		try {
 			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName \r\n" + "FROM seller INNER JOIN department "
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
 							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
 
 			st.setInt(1, id);
