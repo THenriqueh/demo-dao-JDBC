084e5d7a48770253dd38ec733aba3bfb7f829661
Thallis Ferreira <thallis.ferreira2019@gmail.com>
2022-07-28 03:44:58 -0300

    delete implementation

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,6 +2,7 @@ package aplication;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -10,7 +11,7 @@ import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("=== TEST 1 seller findById ===");
 		Seller seller = sellerDao.findById(3);
@@ -31,6 +32,19 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Insered new Id = " + newSeller.getId());
+		
+		System.out.println("=== TEST 5  seller findByDepartment ===");
+		Seller seller2 = new Seller();
+		seller2 = sellerDao.findById(1);
+		seller2.setName("Martha");
+		sellerDao.update(seller2);
+		System.out.println("Update completed");
+		System.out.println("=== TEST 6  seller findByDepartment ===");
+		System.out.println("Enter Id:");
+		int id = sc.nextInt();
+		sellerDao.deletById(id);
+		System.out.println("Delete completed");
+		sc.close();
 	}
 
 }
